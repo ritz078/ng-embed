fa3a1fe2a63502a312d88ab723795c21b93cf519
ritz078 <rkritesh078@gmail.com>
2015-02-24 18:20:54 +0530

    bug-fixed : grunt file updated with correct path

--- GruntFile.js
+++ GruntFile.js
@@ -1,22 +1,20 @@
+'use strict';
 module.exports = function(grunt) {
     grunt.initConfig({
         uglify: {
             options: {
                 mangle: true,
-                compress: true,
-                sourceMap: "dist/ng-emoticons.map"
+                compress: true
             },
             target: {
-                src: "src/ng-emoticons.js",
-                dest: "dist/ng-emoticons.min.js"
+                src: 'src/ng-emoticons.js',
+                dest: 'dist/ng-emoticons.min.js'
             }
         },
         cssmin:{
         	minify:{
-        		expand:true,
-        		src:["src/ng-emoticons.css"],
-        		dest:"dist/",
-        		ext:".min.css"
+        		src:'src/ng-emoticons.css',
+        		dest:'dist/ng-emoticons.min.css'
         	}
         },
          jshint: {
@@ -27,14 +25,14 @@ module.exports = function(grunt) {
                 unused: true
             },
             target: {
-                src: "src/ng-emoticons.js"
+                src: 'src/ng-emoticons.js'
 
             }
         }
     });
 
-    grunt.loadNpmTasks("grunt-contrib-uglify");
-    grunt.loadNpmTasks("grunt-contrib-cssmin");
-    grunt.loadNpmTasks("grunt-contrib-jshint");
-    grunt.registerTask('default', ["jshint","uglify","cssmin"]);
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('default', ['jshint','uglify','cssmin']);
 };
