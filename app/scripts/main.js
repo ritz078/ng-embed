0aa7cbcdc0beed07b796e780a0ac1ff888f770f4
ritz078 <rkritesh078@gmail.com>
2015-03-05 16:05:56 +0530

    feat : web development started

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+angular.module('ngEmoticonsApp',['lumx','ngRoute'])
+
+.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'MainCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+  }])
+
+.controller('ngEmoticonsController',['$scope',function($scope){
+
+  }])
+
+.controller('MainCtrl',['$scope',function($scope){
+
+}]);
+
+
