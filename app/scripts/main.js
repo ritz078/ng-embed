d47ff93d7741e5a782b961c9c32eaf6b3b403b26
ritz078 <rkritesh078@gmail.com>
2015-03-08 05:51:33 +0530

    features added in home page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,17 +12,54 @@ angular.module('ngEmoticonsApp',['lumx','ngRoute'])
         templateUrl: 'views/login.html',
         controller: 'LoginCtrl'
       })
+      .when('/documentation',{
+        templateUrl:'views/documentation.html',
+        controller:'DocumentationCtrl'
+      })
+      .when('/examples',{
+        templateUrl:'views/examples.html',
+        controller:'ExampleCtrl'
+      })
+      .when('/getting-started',{
+        templateUrl:'views/gettingstarted.html',
+        controller:'GettingStartedCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
 
   }])
 
-.controller('ngEmoticonsController',['$scope',function($scope){
+  .service('githubData',['$q','$http',function($q,$http){
+    var deferred=$q.defer();
+    this.getRepoData=function(){
+      $http.get('https://api.github.com/repos/ritz078/ngEmoticons/tags')
+        .success(function(d){
+          deferred.resolve(d);
+          console.log(d);
+        });
+      return deferred.promise;
+    };
+  }])
 
+.controller('ngEmoticonsController',['$scope','githubData',function($scope,githubData){
+    var promise=githubData.getRepoData();
+    promise.then(function(d){
+      $scope.github=d[0];
+    });
   }])
 
 .controller('MainCtrl',['$scope',function($scope){
+
+}])
+
+.controller('ExampleCtrl',['$scope',function($scope){
+
+  }])
+  .controller('DocumentationCtrl',['$scope',function($scope){
+
+  }])
+.controller('GettingStartedCtrl',['$scope',function($scope){
 
 }]);
 
