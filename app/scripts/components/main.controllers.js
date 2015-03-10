98cafac3e953d80ef834b912f3cdb58a05390bf1
ritz078 <rkritesh078@gmail.com>
2015-03-10 21:31:49 +0530

    commmit

--- app/scripts/components/main.controllers.js
+++ app/scripts/components/main.controllers.js
@@ -1,11 +1,22 @@
 'use strict';
 
-angular.module('ngEmoticonsApp').controller('ngEmoticonsController', ['$scope', 'githubData', function ($scope, githubData) {
-  var promise = githubData.getRepoData();
-  promise.then(function (d) {
-    $scope.github = d[0];
-  });
-}])
+angular.module('ngEmoticonsApp')
+
+  .service('emojiList', ['$http', '$q', function ($http, $q) {
+    var deferred = $q.defer();
+    this.getData = function () {
+      $http.get('../data/data.json').success(function (d) {
+        deferred.resolve(d);
+      });
+      return deferred.promise;
+    }
+  }])
+  .controller('ngEmoticonsController', ['$scope', 'githubData', function ($scope, githubData) {
+    var promise = githubData.getRepoData();
+    promise.then(function (d) {
+      $scope.github = d[0];
+    });
+  }])
 
   .controller('MainCtrl', ['$scope', function ($scope) {
 
@@ -14,9 +25,10 @@ angular.module('ngEmoticonsApp').controller('ngEmoticonsController', ['$scope', 
   .controller('ExampleCtrl', ['$scope', function ($scope) {
 
   }])
-  .controller('DocumentationCtrl', ['$scope', function ($scope) {
+  .controller('DocumentationCtrl', ['$scope', '$http', function ($scope, $http) {
+
 
   }])
   .controller('GettingStartedCtrl', ['$scope', function ($scope) {
-$scope.x='```npm install --save emoticons-js```';
+    $scope.x = '```npm install --save emoticons-js```';
   }]);
