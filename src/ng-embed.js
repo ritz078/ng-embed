2057957a753bbd37c140fa456db4421a5a736361
Robert Michalski <robert@surveylegend.com>
2016-12-20 07:39:29 +0100

    Moved code from neGist directive to pure js function in top closure.

--- src/ng-embed.js
+++ src/ng-embed.js
@@ -748,43 +748,45 @@
 
         //This directive is a modification of a module developed by Scott Corgan.
         //present at scottcorgan/angular-gist
-        .directive('neGist', function () {
-            return {
-                restrict: 'EA',
-                replace : true,
-                template: '<div></div>',
-                link    : function (scope, element, attrs) {
-                    var gistId = attrs.id;
+	    .directive('neGist', function () {
+		    return {
+			    restrict: 'EA',
+			    replace : true,
+			    template: '<div></div>',
+			    link    : function (scope, element, attrs) {
+				    appendGistToElement(element[0], attrs.id);
+			    }
+		    };
+	    });
 
-                    var iframe = document.createElement('iframe');
-                    iframe.setAttribute('width', '100%');
-                    iframe.setAttribute('frameborder', '0');
-                    iframe.id = "gist-" + gistId;
-                    element[0].appendChild(iframe);
+    function appendGistToElement(element, gistId) {
+        var iframe = document.createElement('iframe');
+        iframe.setAttribute('width', '100%');
+        iframe.setAttribute('frameborder', '0');
+        iframe.id = "gist-" + gistId;
+element.appendChild(iframe);
 
-                    var iframeHtml = '<html><head><base target="_parent"><style>table{font-size:12px;}</style>' +
-                        '</head><body onload="parent.document.getElementById(\'' + iframe.id + '\').style.height=' +
-                        'document.body.scrollHeight + \'px\'" style="margin:10px 0;"><script type="text/javascript">' +
-                        '!function(){"use strict";window.retargetLinks=function(){ var gists=' +
-                        'document.getElementsByClassName("gist");for(var i=0,links;i<gists.length;i++){' +
-                        'links=gists[i].getElementsByTagName("a");for(var j=0;j<links.length;j++){ ' +
-                        'links[j].setAttribute("target","_blank");}}}}();</script><script type="text/javascript" ' +
-                        'src="https://gist.github.com/' + gistId + '.js" onload="retargetLinks()"></script></body></html>';
+        var iframeHtml = '<html><head><base target="_parent"><style>table{font-size:12px;}</style>' +
+            '</head><body onload="parent.document.getElementById(\'' + iframe.id + '\').style.height=' +
+            'document.body.scrollHeight + \'px\'" style="margin:10px 0;"><script type="text/javascript">' +
+            '!function(){"use strict";window.retargetLinks=function(){ var gists=' +
+            'document.getElementsByClassName("gist");for(var i=0,links;i<gists.length;i++){' +
+            'links=gists[i].getElementsByTagName("a");for(var j=0;j<links.length;j++){ ' +
+            'links[j].setAttribute("target","_blank");}}}}();</script><script type="text/javascript" ' +
+            'src="https://gist.github.com/' + gistId + '.js" onload="retargetLinks()"></script></body></html>';
 
-                    var doc = iframe.document;
-                    if (iframe.contentDocument) {
-                        doc = iframe.contentDocument;
-                    }
-                    else if (iframe.contentWindow) {
-                        doc = iframe.contentWindow.document;
-                    }
+        var doc = iframe.document;
+        if (iframe.contentDocument) {
+            doc = iframe.contentDocument;
+        }
+        else if (iframe.contentWindow) {
+            doc = iframe.contentWindow.document;
+        }
 
-                    doc.open();
-                    doc.writeln(iframeHtml);
-                    doc.close();
-                }
-            };
-        });
+        doc.open();
+        doc.writeln(iframeHtml);
+        doc.close();
+    }
 
 	function getUniqueArray(list) {
 		//*
